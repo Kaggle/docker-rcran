3aeaa2e186b854010d095e63684e2efc5378b4b4
Ben Hamner <ben@benhamner.com>
2015-06-12 14:22:11 -0700

    quieter installs; added timing

--- package_installs.R
+++ package_installs.R
@@ -4,17 +4,17 @@ successes = 0
 errors = 0
 
 my.install.packages <- function(package) {
-    install.packages(package)
+    install.packages(package, verbose=FALSE, quiet=TRUE)
     return("success")
 }
 
 for (package in packages$Package) {
-    cat("Installing Package ", package, "\n")
-    status <- tryCatch(my.install.packages(package),
-                       error=function(e) {
-                       print(e)
-                       cat("Failed to install package ", package, "\n")
-                       return("error")})
+    cat(as.character(Sys.time()), ": Installing Package ", package, "\n")
+    status <- system.time(tryCatch(my.install.packages(package),
+                                   error=function(e) {
+                                   print(e)
+                                   cat("Failed to install package ", package, "\n")
+                                   return("error")}))
     successes <- successes + (if (status=="success") 1 else 0)
     errors    <- errors    + (if (status=="error")   1 else 0)
     cat(successes, "successes and", errors, "errrors so far\n")
