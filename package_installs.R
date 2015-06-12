b66ad5df296e2f4708950218b721308408b8427d
Ben Hamner <ben@benhamner.com>
2015-06-12 11:23:02 -0700

    this should be the right code

--- package_installs.R
+++ package_installs.R
@@ -3,15 +3,19 @@ packages <- as.data.frame(available.packages())
 successes = 0
 errors = 0
 
+my.install.packages <- function(package) {
+    install.packages(package)
+    return("success")
+}
+
 for (package in packages$Package) {
     cat("Installing Package ", package, "\n")
-    tryCatch(function() {
-            install.package(package)
-            successes <- successes + 1},
-        error=function(e) {
-            errors <- errors + 1
-            print(e)
-            cat("Failed to install package ", package, "\n")
-        })
+    status <- tryCatch(my.install.packages(package),
+                       error=function(e) {
+                       print(e)
+                       cat("Failed to install package ", package, "\n")
+                       return("error")})
+    successes <- successes + (if (status=="success") 1 else 0)
+    errors    <- errors    + (if (status=="error")   1 else 0)
     cat(successes, "successes and", errors, "errrors so far\n")
 }
