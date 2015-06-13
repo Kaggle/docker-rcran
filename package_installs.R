c1fdc12bad8d9325c9e7c10c0e3a97e50d79063e
Ben Hamner <ben@benhamner.com>
2015-06-12 17:31:52 -0700

    removed system.time call

--- package_installs.R
+++ package_installs.R
@@ -10,11 +10,11 @@ my.install.packages <- function(package) {
 
 for (package in packages$Package) {
     cat(as.character(Sys.time()), ": Installing Package ", package, "\n")
-    status <- system.time(tryCatch(my.install.packages(package),
-                                   error=function(e) {
-                                   print(e)
-                                   cat("Failed to install package ", package, "\n")
-                                   return("error")}))
+    status <- tryCatch(my.install.packages(package),
+                       error=function(e) {
+                       print(e)
+                       cat("Failed to install package ", package, "\n")
+                       return("error")}))
     successes <- successes + (if (status=="success") 1 else 0)
     errors    <- errors    + (if (status=="error")   1 else 0)
     cat(successes, "successes and", errors, "errrors so far\n")
