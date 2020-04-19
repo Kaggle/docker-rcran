fee121e95755481520d530fc19c067ccb6bcdf5c
Philmod <philmod@google.com>
2020-04-19 19:32:28 -0400

    Only install the packages that are missing

--- package_installs.R
+++ package_installs.R
@@ -130,10 +130,12 @@ while(length(dl) > 0 || length(av) != M) {
 }
 
 # Make sure the packages from the file `packages` are properly installed 
-# by (re-)installing in a single thread, as they sometimes fail in the 
+# otherwise reinstalling in a single thread, as they sometimes fail in the
 # previous technique.
 for (p in p[,1]) {
-  install.packages(p, verbose=FALSE, quiet=FALSE)
+  if (!require(p, character.only = TRUE)) {
+    install.packages(p, verbose=FALSE, quiet=FALSE)
+  }
 }
 
 print("Done!")
