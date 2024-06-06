47012bf5a59caaf91f239c6b2375097a7ba80b28
Jonathan Calderon Chavez <chavezcalderon@google.com>
2024-06-06 04:03:16 +0000

    haccxxxcksss

--- package_installs.R
+++ package_installs.R
@@ -1,50 +1,3 @@
-clean_up_dependencies <- function(x, available = NULL)
-{
-    ## x is a character vector of Depends / Suggests / Imports entries
-    ## returns a character vector of all the package dependencies mentioned
-    x <- x[!is.na(x)]
-    if(!length(x)) return(x)
-    x <- unlist(strsplit(x, ",", fixed = TRUE), use.names = FALSE)
-    unique(sub("^[[:space:]]*([[:alnum:].]+).*$", "\\1" , x))
-}
-
-make_dependency_list <- function(pkgs, available, dependencies = c("Depends", "Imports", "LinkingTo"), recursive = FALSE)
-{
-    ## given a character vector of packages,
-    ## return a named list of character vectors of their dependencies.
-    ## If recursive = TRUE, do this recursively.
-    if(!length(pkgs)) return(NULL)
-    if(is.null(available))
-        stop(gettextf("%s must be supplied", sQuote("available")), domain = NA)
-    info <- available[pkgs, dependencies, drop = FALSE]
-    x <- vector("list", length(pkgs)); names(x) <- pkgs
-    if(recursive) {
-        known <- row.names(available)
-        xx <- vector("list", length(known)); names(xx) <- known
-        info2 <-  available[, dependencies, drop = FALSE]
-        for (i in seq_along(known))
-            xx[[i]] <- clean_up_dependencies(info2[i, ])
-        for (i in pkgs) {
-            p <- xx[[i]]
-            p <- p[p %in% known]; p1 <- p
-            repeat {
-                extra <- unlist(xx[p1])
-                extra <- extra[extra != i]
-                extra <- extra[extra %in% known]
-                deps <- unique(c(p, extra))
-                if (length(deps) <= length(p)) break
-                p1 <- deps[!deps %in% p]
-                p <- deps
-            }
-            x[[i]] <- p
-        }
-    } else {
-        for (i in seq_along(pkgs)) x[[i]] <- clean_up_dependencies(info[i, ])
-    }
-    x
-}
-
-
 # Repo to pull package data and metadata from.
 REPO <- 'http://cran.us.r-project.org'
 options(repos = c("CRAN" = REPO))
@@ -109,6 +62,7 @@ alreadyInstalled <- function(pkg){
 }
 vecAlreadyInstalled <- Vectorize(alreadyInstalled)
 
+source('utils.R')
 print("Generating dependency list...")
 dl <- make_dependency_list(pkgs, allPackages, recursive = TRUE)
 dl <- dl[!vecAlreadyInstalled(names(dl))]
